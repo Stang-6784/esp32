745522946abfa973d48c9779aae99e056863fb8f
Stang-6784 <stangpat.email@gmail.com>
2025-12-03 16:54:57 +0700

    update 3/12/2025

--- src/main.cpp
+++ src/main.cpp
@@ -4,6 +4,7 @@
 #include <Adafruit_Sensor.h>
 #include <Adafruit_GFX.h>
 #include <Adafruit_SSD1306.h>
+#include <RTClib.h>
 
 #define SCREENWIDTH 128 // OLED display width, in pixels
 #define SCREENHEIGHT 64 // OLED display height, in pixels
@@ -11,6 +12,7 @@
 
 Adafruit_BME280 bme; // I2C
 Adafruit_SSD1306 display(SCREENWIDTH, SCREENHEIGHT, &Wire, OLED_RESET);
+RTC_DS3231 rtc;
 
 float tempRead() {
     
@@ -46,12 +48,25 @@ float humiRead() {
 void setup() {
     Serial.begin(115200);
     Wire.begin(21, 22); // SDA, SCL pins for ESP32
+
+	if (!bme.begin(0x76)) {
+		Serial.println("ไม่พบ BME280 ตรวจสอบการเชื่อมต่อ!");
+		while (1);
+	}
     
     if (!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) { // Address 0x3C for 128x64
         Serial.println("ไม่พบจอ OLED ตรวจสอบการเชื่อมต่อ!");
         while (1);
     }
+	if (!rtc.begin()) {
+		Serial.println("ไม่พบ RTC ตรวจสอบการเชื่อมต่อ!");
+		while (1);
+	}
 
+	if (rtc.lostPower()) {
+		Serial.println("RTC ยังไม่ถูกตั้งค่า กำลังตั้งค่าเวลาเริ่มต้น...");
+		rtc.adjust(DateTime(F(__DATE__), F(__TIME__)));
+	}
         // Invert Display
          //display.invertDisplay(1);
 
@@ -67,29 +82,45 @@ void setup() {
 
 
 void loop() {
-    Serial.print("Temperature: ");
-    Serial.print(tempRead());
-    Serial.println(" °C");
-    Serial.println("\n");
-	Serial.print("Humanity:");
-	Serial.print("Humanity:");
-	Serial.print(humiRead());
-	Serial.println(" %");
-	
+	DateTime now = rtc.now();
+	int hour = now.hour();
+	int min = now.minute();
+	int sec = now.second();
+
+	Serial.print("Time: ");
+	if (hour < 10) Serial.print("0");
+	Serial.print(hour);
+	Serial.print(":");
+	if (min < 10) Serial.print("0");
+	Serial.print(min);
+	Serial.print(":");
+	if (sec < 10) Serial.print("0");
+	Serial.println(sec);
+
+	delay(500);
+    // Serial.print("Temperature: ");
+    // Serial.print(tempRead());
+    // Serial.println(" °C");
+    // Serial.println("\n");
+	// Serial.print("Humanity:");
+	// Serial.print("Humanity:");
+	// Serial.print(humiRead());
+	// Serial.println(" %");
+
 	// Clear the buffer.
-    display.clearDisplay();
-	display.setTextSize(2);      // Normal 1:1 pixel scale
+	display.clearDisplay();
+	display.setTextSize(1);      // Normal 1:1 pixel scale
 	display.setTextColor(SSD1306_WHITE); // Draw white text
-	display.setCursor(0,0);     // Start at top-left corner
+	display.setCursor(0, 0);     // Start at top-left corner
 	display.print(tempRead());
 	display.print(" C");
-	display.setCursor(0,30);     // Start at top-left corner
+	display.setCursor(0, 30);     // Start at top-left corner
 	display.print(humiRead());
 	display.print(" %");
+	display.setCursor(0, 40);     // Start at top-left corner
+
     display.display();
 }
-
-
 // int ledPins[] = {23, 22, 21, 18, 19};
 
 // void setup() {
